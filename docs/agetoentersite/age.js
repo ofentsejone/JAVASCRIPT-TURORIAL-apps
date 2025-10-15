8a66f5a2d7cf7906799a2f13e06b02e1c4e2ed78
Ofentse Makuwa <ofentsejone@gmail.com>
2025-10-15 10:16:37 +0200

    Add files via upload

---
+++ docs/agetoentersite/age.js
@@ -0,0 +1,16 @@
+let age;
+ 
+document.getElementById('submit').onclick = function(){
+    age=document.getElementById('user-age').value;
+    age=Number(age);
+
+   if(age <= 18)
+   {document.getElementById('results').textContent="You are still young to enter";}
+   else if(age>=100)
+    {document.getElementById('results').textContent="You are too old enough  enter";}
+   else if(age==0)
+    {document.getElementById('results').textContent="You cant  enter";}
+
+// else{document.getElementById('results').textContent=`Welcome to the site`;}
+   
+}
